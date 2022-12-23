36e6881270428f6a08b47ad96a90875cbcbb8f29
shermai2022 <115166014+shermai2022@users.noreply.github.com>
2022-12-23 14:15:01 +0300

    Create testrun.py

---
+++ lab4/testrun.py
@@ -0,0 +1,19 @@
+from figure import *
+
+circle_area()
+circle_area(7)
+
+circle_perimeter()
+circle_perimeter(9)
+
+triangle_perimeter()
+triangle_perimeter(4,3,7)
+
+triangle_area()
+triangle_area(1,9,6)
+
+square_perimeter()
+square_perimeter(2)
+
+square_area()
+square_area(7)
