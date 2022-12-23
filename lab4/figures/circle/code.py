27f7f694ba5f64b816e96b0052c1c878a5515ff6
shermai2022 <115166014+shermai2022@users.noreply.github.com>
2022-12-23 14:18:29 +0300

    Create code.py

---
+++ lab4/figures/circle/code.py
@@ -0,0 +1,11 @@
+pi = 3.1415
+
+def circle_area(default_radius = 5):
+    A = (pi *  default_radius ** 2)
+    print(A)
+    return
+    
+def circle_perimeter(default_radius = 5):
+    circum = 2 * pi * default_radius
+    print(circum)
+    return
