9f7d5cd7d7f9341ba9d9e28eafc8585830e0a950
shermai2022 <115166014+shermai2022@users.noreply.github.com>
2022-12-23 14:13:47 +0300

    Create task4.py

---
+++ lab4/task4.py
@@ -0,0 +1,15 @@
+A = {'name': "aaron",
+ 'networth': "$32B",
+ 'company':"Aerobabel"}
+
+def slavar(dict, key):
+    
+    b = dict[key]
+    print(b)
+    
+    return
+    
+    
+    
+    
+slavar(A, 'name')
