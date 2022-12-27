6e1fc1302e8a158b045223bb4bf9667e588cc8e7
shermai2022 <115166014+shermai2022@users.noreply.github.com>
2022-12-27 21:27:25 +0300

    Update __init__.py

--- lab4/figures/circle/__init__.py
+++ lab4/figures/circle/__init__.py
@@ -1,1 +1,8 @@
-
+import math
+__default_radius = 5
+def circle_perimeter(radius=__default_radius):
+    C = 2*math.pi*radius
+    print(C)
+def circle_area(radius=__default_radius):
+    A = math.pi*pow(radius, 2)
+    print(A)
